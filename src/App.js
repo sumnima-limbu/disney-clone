2ec9975570b459c7c882d442739f4f0e144c1ea9
sumnima-limbu <sumnimaklimbu@gmail.com>
2023-07-15 17:27:02 +0545

    add user login and sign out

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Header />
         <Routes>
           <Route exact path="/login" element={<Login />} />
-          <Route exact path="/detail" element={<Detail />} />
+          <Route exact path="/detail/:id" element={<Detail />} />
           <Route exact path="/" element={<Home />} />
         </Routes>
       </Router>
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import { selectMovies } from "../features/movie/movieSlice";
 import { useSelector } from "react-redux";
 
@@ -13,7 +14,9 @@ const Movies = () => {
         {movies &&
           movies.map((movie) => (
             <Wrap key={movie.id}>
-              <img src={movie.CardImg} />
+              <Link to={`/detail/${movie.id}`}>
+                <img src={movie.CardImg} />
+              </Link>
             </Wrap>
           ))}
       </Content>
